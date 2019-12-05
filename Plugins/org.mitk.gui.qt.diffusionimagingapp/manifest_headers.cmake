61f9998dc554fdb2a3e3b978f4f9ee1cf9e9bc7b
Peter Neher <p.neher@dkfz-heidelberg.de>
2019-12-05 09:29:20 +0100

    MBI -> MIC

--- Plugins/org.mitk.gui.qt.diffusionimagingapp/manifest_headers.cmake
+++ Plugins/org.mitk.gui.qt.diffusionimagingapp/manifest_headers.cmake
@@ -1,6 +1,6 @@
 set(Plugin-Name "MITK Diffusion")
 set(Plugin-Version "1.0")
-set(Plugin-Vendor "DKFZ, Medical and Biological Informatics")
+set(Plugin-Vendor "DKFZ, Medical Image Computing")
 set(Plugin-ContactAddress "http://www.mitk.org")
 set(Require-Plugin org.mitk.gui.qt.ext)
 set(Plugin-Activator QmitkDiffusionImagingAppApplicationPlugin)
