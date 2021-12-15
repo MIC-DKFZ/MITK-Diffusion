71ac9a636bbcac93f6cf3d61a04f35b0ca5b19ea
Peter Neher <p.neher@dkfz-heidelberg.de>
2021-12-15 10:16:00 +0100

    Added distance image output

--- Modules/DiffusionCmdApps/FiberProcessing/DistanceToSegmentation.cpp
+++ Modules/DiffusionCmdApps/FiberProcessing/DistanceToSegmentation.cpp
@@ -23,6 +23,7 @@ See LICENSE.txt or http://www.mitk.org for details.
 #include <itkDistanceFromSegmentationImageFilter.h>
 #include <itkTractDensityImageFilter.h>
 #include <mitkDiffusionVersion.h>
+#include <boost/date_time.hpp>
 
 typedef itk::Image<float, 3>    ItkFloatImgType;
 
@@ -40,7 +41,8 @@ int main(int argc, char* argv[])
   parser.setArgumentPrefix("--", "-");
   parser.addArgument("tracts", "", mitkDiffusionCommandLineParser::String, "Tracts:", "input fiber tracts (as tractogram or as tract density iumage (TDI))", us::Any(), false, false, false, mitkDiffusionCommandLineParser::Input);
   parser.addArgument("surface", "", mitkDiffusionCommandLineParser::String, "Segmentation:", "input segmentation mesh (.vtp)", us::Any(), false, false, false, mitkDiffusionCommandLineParser::Input);
-  parser.addArgument("out_file", "", mitkDiffusionCommandLineParser::String, "Output:", "output text file", us::Any(), false, false, false, mitkDiffusionCommandLineParser::Output);
+  parser.addArgument("out_dists", "", mitkDiffusionCommandLineParser::String, "Output:", "output text file", us::Any(), true, false, false, mitkDiffusionCommandLineParser::Output);
+  parser.addArgument("out_image", "", mitkDiffusionCommandLineParser::String, "Output:", "output image", us::Any(), true, false, false, mitkDiffusionCommandLineParser::Output);
   parser.addArgument("thresholds", "", mitkDiffusionCommandLineParser::StringList, "Thresholds:", "distances for which voxels are counted", us::Any(), true, false, false, mitkDiffusionCommandLineParser::Input);
 
   std::map<std::string, us::Any> parsedArgs = parser.parseArguments(argc, argv);
@@ -49,7 +51,14 @@ int main(int argc, char* argv[])
 
   std::string in_tracts = us::any_cast<std::string>(parsedArgs["tracts"]);
   std::string in_seg = us::any_cast<std::string>(parsedArgs["surface"]);
-  std::string outImageFile = us::any_cast<std::string>(parsedArgs["out_file"]);
+
+  std::string out_dists = "";
+  if (parsedArgs.count("out_dists"))
+    out_dists = us::any_cast<std::string>(parsedArgs["out_dists"]);
+
+  std::string out_image = "";
+  if (parsedArgs.count("out_image"))
+    out_image = us::any_cast<std::string>(parsedArgs["out_image"]);
 
   std::vector< std::string > thresholds_str = {"0.0", "3.0", "5.0", "7.0"};
   std::vector< float > thresholds;
@@ -89,45 +98,57 @@ int main(int argc, char* argv[])
     filter->SetSegmentationSurface(inputSeg);
     filter->SetThresholds(thresholds);
     filter->Update();
-//    auto outImg = filter->GetOutput();
 
     auto volume = inputItkTDI->GetSpacing()[0]*inputItkTDI->GetSpacing()[1]*inputItkTDI->GetSpacing()[2];
 
 
-
-    std::ofstream statistics_file;
-    statistics_file.open(outImageFile, std::ios_base::out | std::ios_base::app);
-
-
-    statistics_file << "MITK Diffusion git commit hash: " << MITKDIFFUSION_REVISION << std::endl;
-    statistics_file << "MITK Diffusion branch name: " << MITKDIFFUSION_REVISION_NAME << std::endl;
-    statistics_file << "MITK git commit hash: " << MITK_REVISION << std::endl;
-    statistics_file << "MITK branch name: " << MITK_REVISION_NAME << std::endl;
-
-    statistics_file << "Tractogram file: " << in_tracts << std::endl;
-    statistics_file << "Segmentation surface file: " << in_seg << std::endl;
-
+    MITK_INFO << "Tractogram file: " << in_tracts;
+    MITK_INFO << "Segmentation surface file: " << in_seg;
     MITK_INFO << "Reference image spacing: " << inputItkTDI->GetSpacing();
-    statistics_file << "Reference image spacing: " << inputItkTDI->GetSpacing() << std::endl;
-
     MITK_INFO << "Distance between surface and closest fiber-containing voxel-center: " << filter->GetMinDistance() << " mm";
-    statistics_file << "Distance between surface and closest fiber-containing voxel-center: " << filter->GetMinDistance() << " mm" << std::endl;
 
     for (unsigned int i=0; i<filter->GetThresholds().size(); ++i)
-    {
       MITK_INFO << "Voxels closer than " << filter->GetThresholds().at(i) << " mm: " << filter->GetCounts().at(i) << " (" << filter->GetCounts().at(i)*volume << " mm³)";
-      statistics_file << "Voxels closer than " << filter->GetThresholds().at(i) << " mm: " << filter->GetCounts().at(i) << " (" << filter->GetCounts().at(i)*volume << " mm³)" << std::endl;
+
+    if (!out_dists.empty())
+    {
+      MITK_INFO << "Saving data to text file: " << out_dists;
+      std::ofstream statistics_file;
+      statistics_file.open(out_dists, std::ios_base::out | std::ios_base::app);
+
+      statistics_file << "MITK Diffusion git commit hash: " << MITKDIFFUSION_REVISION << std::endl;
+      statistics_file << "MITK Diffusion branch name: " << MITKDIFFUSION_REVISION_NAME << std::endl;
+      statistics_file << "MITK git commit hash: " << MITK_REVISION << std::endl;
+      statistics_file << "MITK branch name: " << MITK_REVISION_NAME << std::endl;
+
+      boost::posix_time::ptime timeLocal = boost::posix_time::second_clock::local_time();
+      statistics_file << "Current System Time = " << timeLocal << std::endl;
+
+      statistics_file << "Tractogram file: " << in_tracts << std::endl;
+      statistics_file << "Segmentation surface file: " << in_seg << std::endl;
+
+      statistics_file << "Reference image spacing: " << inputItkTDI->GetSpacing() << std::endl;
+
+      statistics_file << "Distance between surface and closest fiber-containing voxel-center: " << filter->GetMinDistance() << " mm" << std::endl;
+
+      for (unsigned int i=0; i<filter->GetThresholds().size(); ++i)
+        statistics_file << "Voxels closer than " << filter->GetThresholds().at(i) << " mm: " << filter->GetCounts().at(i) << " (" << filter->GetCounts().at(i)*volume << " mm³)" << std::endl;
+
+      statistics_file << "-------------------------------------------------\n\n" << std::endl;
+      statistics_file.close();
     }
 
-    statistics_file << "-------------------------------------------------\n\n" << std::endl;
-    statistics_file.close();
+    // save image containing voxel-wise distances
+    if (!out_image.empty())
+    {
+      MITK_INFO << "Saving voxel-wise distances to image file: " << out_image;
 
-//    // get output image
-//    mitk::Image::Pointer img = mitk::Image::New();
-//    img->InitializeByItk(outImg);
-//    img->SetVolume(outImg->GetBufferPointer());
-
-//    mitk::IOUtil::Save(img, outImageFile );
+      auto outImg = filter->GetOutput();
+      mitk::Image::Pointer img = mitk::Image::New();
+      img->InitializeByItk(outImg);
+      img->SetVolume(outImg->GetBufferPointer());
+      mitk::IOUtil::Save(img, out_image );
+    }
   }
   catch (const itk::ExceptionObject& e)
   {
