041b4a04777eaf0b2b9111780ab94b5b32caf4c6
Peter Neher <p.neher@dkfz-heidelberg.de>
2023-06-27 12:10:00 +0200

    remove python from manifest headers

--- Plugins/org.mitk.gui.qt.diffusionimagingapp/manifest_headers.cmake
+++ Plugins/org.mitk.gui.qt.diffusionimagingapp/manifest_headers.cmake
@@ -2,7 +2,7 @@ set(Plugin-Name "MITK Diffusion")
 set(Plugin-Version "1.0")
 set(Plugin-Vendor "DKFZ, Medical Image Computing")
 set(Plugin-ContactAddress "http://www.mitk.org")
-set(Require-Plugin org.mitk.gui.qt.ext org.mitk.gui.qt.diffusionimaging.controls org.mitk.gui.qt.diffusionimaging.denoising org.mitk.gui.qt.diffusionimaging.fiberfox org.mitk.gui.qt.diffusionimaging.fiberprocessing org.mitk.gui.qt.diffusionimaging.ivim org.mitk.gui.qt.diffusionimaging.odfpeaks org.mitk.gui.qt.diffusionimaging.partialvolume org.mitk.gui.qt.diffusionimaging.preprocessing org.mitk.gui.qt.diffusionimaging.python org.mitk.gui.qt.diffusionimaging.reconstruction org.mitk.gui.qt.diffusionimaging.registration org.mitk.gui.qt.diffusionimaging.tractography)
+set(Require-Plugin org.mitk.gui.qt.ext org.mitk.gui.qt.diffusionimaging.controls org.mitk.gui.qt.diffusionimaging.denoising org.mitk.gui.qt.diffusionimaging.fiberfox org.mitk.gui.qt.diffusionimaging.fiberprocessing org.mitk.gui.qt.diffusionimaging.ivim org.mitk.gui.qt.diffusionimaging.odfpeaks org.mitk.gui.qt.diffusionimaging.partialvolume org.mitk.gui.qt.diffusionimaging.preprocessing org.mitk.gui.qt.diffusionimaging.reconstruction org.mitk.gui.qt.diffusionimaging.registration org.mitk.gui.qt.diffusionimaging.tractography)
 set(Plugin-Activator QmitkDiffusionImagingAppApplicationPlugin)
 
 
