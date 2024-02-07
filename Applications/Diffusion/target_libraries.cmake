59ee1a2a9b6c486111647962cd48fdfe86f3f2c9
Peter Neher <p.neher@dkfz-heidelberg.de>
2024-02-07 16:03:29 +0100

    add target_libraries file

---
+++ Applications/Diffusion/target_libraries.cmake
@@ -0,0 +1,38 @@
+# A list of plug-in targets which should be automatically enabled
+# (or be available in external projects) for this application
+
+set(target_libraries
+  org_blueberry_ui_qt
+  org_blueberry_ui_qt_help
+  org_mitk_planarfigure
+  org_mitk_gui_qt_ext
+  org_mitk_gui_qt_datamanager
+  org_mitk_gui_qt_segmentation
+  org_mitk_gui_qt_volumevisualization
+  org_mitk_gui_qt_diffusionimaging_controls
+  org_mitk_gui_qt_diffusionimaging_fiberfox
+  org_mitk_gui_qt_diffusionimaging_fiberprocessing
+  org_mitk_gui_qt_diffusionimaging_ivim
+  org_mitk_gui_qt_diffusionimaging_odfpeaks
+  org_mitk_gui_qt_diffusionimaging_preprocessing
+  org_mitk_gui_qt_diffusionimaging_reconstruction
+  org_mitk_gui_qt_diffusionimaging_tractography
+  org_mitk_gui_qt_diffusionimaging_registration
+  org_mitk_gui_qt_diffusionimaging_denoising
+  org_mitk_gui_qt_diffusionimaging_partialvolume
+  org_mitk_gui_qt_diffusionimagingapp
+  org_mitk_gui_qt_matchpoint_algorithm_browser
+  org_mitk_gui_qt_matchpoint_algorithm_control
+  org_mitk_gui_qt_matchpoint_mapper
+  org_mitk_gui_qt_imagenavigator
+  org_mitk_gui_qt_moviemaker
+  org_mitk_gui_qt_measurementtoolbox
+  org_mitk_gui_qt_basicimageprocessing
+  org_mitk_gui_qt_viewnavigator
+  org_mitk_gui_qt_renderwindowmanager
+)
+
+if(MITK_USE_Python3)
+  list(APPEND target_libraries org_mitk_gui_qt_diffusionimaging_python)
+  list(APPEND target_libraries org_mitk_gui_qt_python)
+endif()
